a2b6ee576c054a45e9b0c67e133718656f9744df
= <sannatwill@gmail.com>
2024-02-12 15:37:33 +0000

    Improved robot arm smoothness
    Edited controlRate & no. of increments

--- robotArmPositionControl/robotArmPositionControl.ino
+++ robotArmPositionControl/robotArmPositionControl.ino
@@ -22,8 +22,8 @@ float s3Angle;
 float s3AnglePrev;
 float s3AngleMax;
 int s3Width;
-float coordinatesInitial[] = {17.2, 5.7, 0.0}; // {x, y, z}
-float coordinatesFinal[] = {17.2, -4.3, 0.0}; // {x, y, z}
+float coordinatesInitial[] = {15.2, 5.7, 0.0}; // {x, y, z}
+float coordinatesFinal[] = {18.2, -4.3, 0.0}; // {x, y, z}
 float xCurr;
 float yCurr;
 float zCurr;
@@ -34,18 +34,17 @@ const float L3 = 13.5;
 
 unsigned long timeStamp;
 unsigned long elapsedTime;
-unsigned long controlRate = 1000; // milliseconds
+unsigned long controlRate = 30; // milliseconds
 // float radIncrementMax = (PI/3.0)*(controlRate/1000.0);
 float radIncrementMax = 0.1;
-float yPositionIncrement = (coordinatesFinal[1]-coordinatesInitial[1]) / 20;
+float yPositionIncrement = (coordinatesFinal[1]-coordinatesInitial[1]) / 30;
+float xPositionIncrement = (coordinatesFinal[0]-coordinatesInitial[0]) / 30;
 
 int moveServo(Servo servo, int refPulseWidth, float servoRadAngle);
 float rateLimiter(float targetAngle, float currentAngle, float increment);
 
 // initialise the servo and the serial monitor:
 void setup() {
-  delay(500);
-  
   timeStamp = millis();
   
   s1.attach(s1PWM);  
@@ -55,14 +54,51 @@ void setup() {
   xCurr = coordinatesInitial[0];
   yCurr = coordinatesInitial[1];
   zCurr = coordinatesInitial[2];
+
+  s1Angle = 0;
+  s3Angle = -acos((sq(xCurr) + sq(yCurr) - sq(L2) - sq(L3))/(2*L2*L3));
+  s2Angle = atan2(yCurr, xCurr) - atan2(L3*sin(s3Angle), L2+L3*cos(s3Angle));
+  s1Width = moveServo(s1, 1503, s1Angle);
+  s2Width = moveServo(s2, 1415, s2Angle);
+  s3Width = moveServo(s3, 1345, s3Angle);
+
+  delay(3000);
   
   Serial.begin(9600);
 }
  
 void loop() {
   // Calculating required S2 and S3 angles for the range of vertical line co ordinates (Up and Down)
+  while (xCurr < coordinatesFinal[0]){
+    s1Angle = 0;
+    s3Angle = -acos((sq(xCurr) + sq(yCurr) - sq(L2) - sq(L3))/(2*L2*L3));
+    s2Angle = atan2(yCurr, xCurr) - atan2(L3*sin(s3Angle), L2+L3*cos(s3Angle));
+    
+    // give the servo some time to respond (milliseconds):
+    elapsedTime = millis() - timeStamp;
+    if (elapsedTime >= controlRate) {
+      
+      // anything here will execute at the "controlRate"  
+      
+      // write the servo angle to the servo:
+      s1Width = moveServo(s1, 1503, s1Angle);
+      s2Width = moveServo(s2, 1415, s2Angle);
+      s3Width = moveServo(s3, 1345, s3Angle);
+      Serial.println(s1Angle);
+      Serial.println(s2Angle);
+      Serial.println(s3Angle);
+      Serial.print("\n\n");
+
+      xCurr += xPositionIncrement;
   
-  while (yCurr >= coordinatesFinal[1]){
+      // update the timestamp each time this executes:
+      timeStamp = millis();
+    }
+  }
+
+  delay(1000);
+  
+  while (yCurr > coordinatesFinal[1]){
     s1Angle = 0;
     s3Angle = -acos((sq(xCurr) + sq(yCurr) - sq(L2) - sq(L3))/(2*L2*L3));
     s2Angle = atan2(yCurr, xCurr) - atan2(L3*sin(s3Angle), L2+L3*cos(s3Angle));
