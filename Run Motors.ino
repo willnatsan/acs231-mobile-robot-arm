360be6b63126c0c05d6917c8bf96c62e5d4f6985
RA2004 <89916025+RA2004@users.noreply.github.com>
2024-02-24 13:39:53 +0000

    Update Run Motors.ino

--- Run Motors.ino
+++ Run Motors.ino
@@ -1,17 +1,18 @@
+/***MAKE SURE PWM PINS ARE CONNECTED ONLY TO ~ ONES ON ARDUINO
 
+/***Left Motor Declarations***/
 unsigned char pwmValueL = 125;
 const int pinAI1L = 7;       // Pin allocation for AI1
 const int pinAI2L = 8;       // Pin allocation for AI2
 const int pinPWMAL = 5;       // Pin allocation for the PWM pin
-
-unsigned char pwmValueR = 125;
-const int pinBI1R = 10;       // Pin allocation for AI1
-const int pinBI2R = 11;       // Pin allocation for AI2
-const int pinPWMBR = 12;       // Pin allocation for the PWM pin
-
 boolean AI1L = 0;            // AI1 pin value
 boolean AI2L = 0;            // AI2 pin value
 
+/***Right Motor Declarations***/
+unsigned char pwmValueR = 125;
+const int pinBI1R = 10;       // Pin allocation for AI1
+const int pinBI2R = 11;       // Pin allocation for AI2
+const int pinPWMBR = 3;       // Pin allocation for the PWM pin
 boolean BI1R = 0;            // AI1 pin value
 boolean BI2R = 0;            // AI2 pin value
 
@@ -34,38 +35,37 @@ void setup(void) {
   pinMode(pinStandBy, OUTPUT);
   standBy = true;
   digitalWrite(pinStandBy, standBy);
-  AI1L = false;
-  AI2L = true;
-  BI1R = true;
-  BI2R = false;
+  AI1L = true;
+  AI2L = false;
+
+  BI1R = false;
+  BI2R = true;
 }
  
 void loop(void) {
-    // AI1 = true;
-    // AI2 = false;
-    AI1L = false;
-    AI2L = true;
-    BI1R = true;
-    BI2R = false;
-  digitalWrite(pinAI1L, AI1L);
-  digitalWrite(pinAI2L, AI2L);
+ 
+  // we'll need to change the range from the analog reading (0-1023) down to the range
+  // used by analogWrite (0-255) with map!
+  //LEDbrightness = map(potReading*1, 0, 1023, 0, 255);
+    AI1L = true;
+    AI2L = false;
+
+    BI1R = false;
+    BI2R = true;
+  // digitalWrite(pinAI1L, AI1L);
+  // digitalWrite(pinAI2L, AI2L);
+  // Serial.print("PWM = ");
+  // Serial.println(pwmValueL);
+  // analogWrite(pinPWMAL, pwmValueL);
+
   digitalWrite(pinBI1R, BI1R);
   digitalWrite(pinBI2R, BI2R);
   Serial.print("PWM = ");
-  Serial.println(pwmValueL);
-  analogWrite(pinPWMAL, pwmValueL);
+  Serial.println(pwmValueR);
   analogWrite(pinPWMBR, pwmValueR);
   // LED gets brighter the harder you press
   //analogWrite(LEDpin, LEDbrightness); // here you are using PWM !!!
- 
-  delay(500);
 }
-
-
-
-
-
-
 
 
 
