bac62ed88bd60f65c567e238fae346b8b543f2f0
RA2004 <89916025+RA2004@users.noreply.github.com>
2024-02-27 11:55:24 +0000

    Left and Right Motors were mixed up causing robot to turn in opposite direction based when sensing white. Fixed

--- Line_Follower_Motor_Control.ino
+++ Line_Follower_Motor_Control.ino
@@ -12,19 +12,19 @@ int RSreading = 0;
 
 /***Left Motor Declarations***/
 unsigned char pwmValueL = 125;
-const int pinAI1L = 7;       // Pin allocation for AI1
-const int pinAI2L = 8;       // Pin allocation for AI2
-const int pinPWMAL = 5;       // Pin allocation for the PWM pin
-boolean AI1L = 0;            // AI1 pin value
-boolean AI2L = 0;            // AI2 pin value
+const int pinAI1R = 7;       // Pin allocation for AI1
+const int pinAI2R = 8;       // Pin allocation for AI2
+const int pinPWMAR = 5;       // Pin allocation for the PWM pin
+boolean AI1R = 0;            // AI1 pin value
+boolean AI2R = 0;            // AI2 pin value
 
 /***Right Motor Declarations***/
 unsigned char pwmValueR = 125;
-const int pinBI1R = 10;       // Pin allocation for AI1
-const int pinBI2R = 11;       // Pin allocation for AI2
-const int pinPWMBR = 3;       // Pin allocation for the PWM pin
-boolean BI1R = 0;            // AI1 pin value
-boolean BI2R = 0;            // AI2 pin value
+const int pinBI1L = 10;       // Pin allocation for AI1
+const int pinBI2L = 11;       // Pin allocation for AI2
+const int pinPWMBL = 3;       // Pin allocation for the PWM pin
+boolean BI1L = 0;            // AI1 pin value
+boolean BI2L = 0;            // AI2 pin value
 
 const int pinStandBy = 9;   // Pin allocation for the standby pin
 boolean standBy = 0;        // standBy pin Value
@@ -41,34 +41,31 @@ void setup() {
   pinMode(1,INPUT);
   pinMode(2,INPUT);
 
-  pinMode(pinAI1L, OUTPUT);
-  pinMode(pinAI2L, OUTPUT);
-  pinMode(pinPWMAL, OUTPUT);
+  pinMode(pinAI1R, OUTPUT);
+  pinMode(pinAI2R, OUTPUT);
+  pinMode(pinPWMAR, OUTPUT);
 
-  pinMode(pinBI1R, OUTPUT);
-  pinMode(pinBI2R, OUTPUT);
-  pinMode(pinPWMBR, OUTPUT);
+  pinMode(pinBI1L, OUTPUT);
+  pinMode(pinBI2L, OUTPUT);
+  pinMode(pinPWMBL, OUTPUT);
 
   pinMode(pinStandBy, OUTPUT);
   standBy = true;
   digitalWrite(pinStandBy, standBy);
-  AI1L = true;
-  AI2L = false;
+  AI1R = false;
+  AI2R = true;
 
-  BI1R = false;
-  BI2R = true;
+  BI1L = true;
+  BI2L = false;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    AI1L = true;
-    AI2L = false;
-    BI1R = false;
-    BI2R = true;
-    digitalWrite(pinAI1L, AI1L);
-    digitalWrite(pinAI2L, AI2L);
-    digitalWrite(pinBI1R, BI1R);
-    digitalWrite(pinBI2R, BI2R);
+
+    digitalWrite(pinAI1R, AI1R);
+    digitalWrite(pinAI2R, AI2R);
+    digitalWrite(pinBI1L, BI1L);
+    digitalWrite(pinBI2L, BI2L);
     //Read values from sensors
     LSreading = analogRead(LeftSensor);    
     CSreading = analogRead(CentreSensor);
@@ -83,35 +80,24 @@ void loop() {
 
     Serial.print("Right Sensor Reading = ");
     Serial.println(RSreading);
-
+    analogWrite(pinPWMBL,50);
     // analogWrite(pinPWMAL, 120);
     // analogWrite(pinPWMBR, 120);
     if(CSreading > 600){
-      analogWrite(pinPWMAL, 120);
-      analogWrite(pinPWMBR, 120);
+      analogWrite(pinPWMAR, 120);
+      analogWrite(pinPWMBL, 120);
     }
-    else if(LSreading>600 && RSreading < 400){     
-      analogWrite(pinPWMAL, 75);
-      analogWrite(pinPWMBR, 120);
+    else if(RSreading < 400){     
+      analogWrite(pinPWMAR, 120);
+      analogWrite(pinPWMBL, 50);
+
     }
-    else if(RSreading>600 && LSreading < 400){
-      analogWrite(pinPWMAL, 125);
-      analogWrite(pinPWMBR, 75);
+    else if(LSreading < 400){
+      analogWrite(pinPWMAR, 50);
+      analogWrite(pinPWMBL, 120);
     }
     else{
-      analogWrite(pinPWMAL, 120);
-      analogWrite(pinPWMBR, 120);
+      analogWrite(pinPWMAR, 50);
+      analogWrite(pinPWMBL, 50);
     }
-
-    //Display sensor readings
-    // Serial.print("Left Sensor Reading = ");
-    // Serial.print(LSreading);
-    // Serial.print("\t");
-
-    // Serial.print("Centre Sensor Reading = ");
-    // Serial.print(CSreading);
-    // Serial.print("\t");
-
-    // Serial.print("Right Sensor Reading = ");
-    // Serial.println(RSreading);
 }
